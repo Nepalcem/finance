61e9f8e3d0ab6c7f5531f47c96975eef6ccd852b
Nepalcem <mark.emailtest@gmail.com>
2023-10-30 00:40:32 +0200

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
 
-function App() {
+const App = () => {
+  const [quotes, setQuotes] = useState([]);
+console.log(quotes);
+  useEffect(() => {
+    const socket = io("http://localhost:4000");
+
+    socket.on("quotes", (newQuotes) => {
+      setQuotes(newQuotes);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Stock Quotes</h1>
+      <ul>
+        {quotes.map((quote, index) => (
+          <li key={index}>
+            <strong>Ticker:</strong> {quote.ticker},<strong> Price:</strong>{" "}
+            {quote.price},<strong> Change:</strong> {quote.change},
+            <strong> Change Percent:</strong> {quote.change_percent}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,18 @@
-'use strict';
-const express = require('express');
-const http = require('http');
-const io = require('socket.io');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+const cors = require("cors");
 
-const FETCH_INTERVAL = 5000;
-const PORT = process.env.PORT || 4000;
 
 const tickers = [
-  'AAPL', // Apple
-  'GOOGL', // Alphabet
-  'MSFT', // Microsoft
-  'AMZN', // Amazon
-  'FB', // Facebook
-  'TSLA', // Tesla
+  "AAPL", // Apple
+  "GOOGL", // Alphabet
+  "MSFT", // Microsoft
+  "AMZN", // Amazon
+  "FB", // Facebook
+  "TSLA", // Tesla
 ];
-
 function randomValue(min = 0, max = 1, precision = 0) {
   const random = Math.random() * (max - min) + min;
   return random.toFixed(precision);
@@ -23,14 +20,20 @@ function randomValue(min = 0, max = 1, precision = 0) {
 
 function utcDate() {
   const now = new Date();
-  return new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+  return new Date(
+    now.getUTCFullYear(),
+    now.getUTCMonth(),
+    now.getUTCDate(),
+    now.getUTCHours(),
+    now.getUTCMinutes(),
+    now.getUTCSeconds()
+  );
 }
 
-function getQuotes(socket) {
-
-  const quotes = tickers.map(ticker => ({
+function getQuotes() {
+  return tickers.map((ticker) => ({
     ticker,
-    exchange: 'NASDAQ',
+    exchange: "NASDAQ",
     price: randomValue(100, 300, 2),
     change: randomValue(0, 200, 2),
     change_percent: randomValue(0, 1, 2),
@@ -38,44 +41,51 @@ function getQuotes(socket) {
     yield: randomValue(0, 2, 2),
     last_trade_time: utcDate(),
   }));
-
-  socket.emit('ticker', quotes);
 }
 
 function trackTickers(socket) {
-  // run the first time immediately
-  getQuotes(socket);
-
   // every N seconds
-  const timer = setInterval(function() {
-    getQuotes(socket);
-  }, FETCH_INTERVAL);
+  const timer = setInterval(function () {
+    const quotes = getQuotes();
+    socket.emit("quotes", quotes);
+  }, TIME_INTERVAL);
 
-  socket.on('disconnect', function() {
+  socket.on("disconnect", function () {
     clearInterval(timer);
   });
 }
 
+
 const app = express();
-app.use(cors());
 const server = http.createServer(app);
-
-const socketServer = io(server, {
+const io = socketIo(server, {
   cors: {
-    origin: "*",
-  }
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+
+const PORT = process.env.PORT || 4000;
+const TIME_INTERVAL = 5000; // 5 seconds
+
+
+app.use(cors());
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-socketServer.on('connection', (socket) => {
-  socket.on('start', () => {
-    trackTickers(socket);
+io.on("connection", (socket) => {
+  console.log("Client connected");
+  trackTickers(socket);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 });
 
 server.listen(PORT, () => {
-  console.log(`Streaming service is running on http://localhost:${PORT}`);
+  console.log(`Socket server is running on http://localhost:${PORT}`);
 });
