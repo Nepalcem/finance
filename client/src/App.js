7ba94b1e0f35888c5c5410fa9d157818175d7e24
Nepalcem <mark.emailtest@gmail.com>
2023-10-31 02:43:00 +0200

    one chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,77 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
+import { defaultState } from "./components/defaultState";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  scales: {
+    y: {
+      min: 0, // Set the minimum value for y-axis
+      max: 500, // Set the maximum value for y-axis
+      beginAtZero: true,
+      title: {
+        display: true,
+        text: "Price",
+      },
+    },
+  },
+};
+
 
 const App = () => {
-  const [quotes, setQuotes] = useState([]);
-console.log(quotes);
-  useEffect(() => {
-    const socket = io("http://localhost:4000");
+  const [data, setData] = useState(defaultState);
 
-    socket.on("quotes", (newQuotes) => {
-      setQuotes(newQuotes);
-    });
+ useEffect(() => {
+   const socket = io("http://localhost:4000");
 
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+   socket.on("quotes", (newQuotes) => {
+     setData((prevData) => [...prevData, newQuotes[0]]);
+   });
+
+   return () => {
+     socket.disconnect();
+   };
+ }, []);
+  
+  const lastTradeTimes = data.map((item) => item.last_trade_time);
+  const pricesArr = data.map((item) => Number(item.price));
+
+const chartData = {
+  labels: lastTradeTimes,
+  datasets: [
+    {
+      label: "Prices",
+      data: pricesArr,
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ],
+};
 
   return (
-    <div>
+    <div style={{ textAlign: "center", width: "50%", margin:"0 auto" }}>
       <h1>Stock Quotes</h1>
-      <ul>
-        {quotes.map((quote, index) => (
-          <li key={index}>
-            <strong>Ticker:</strong> {quote.ticker},<strong> Price:</strong>{" "}
-            {quote.price},<strong> Change:</strong> {quote.change},
-            <strong> Change Percent:</strong> {quote.change_percent}
-          </li>
-        ))}
-      </ul>
+      <Line options={options} data={chartData} width={600} height={300} />
     </div>
   );
 };
